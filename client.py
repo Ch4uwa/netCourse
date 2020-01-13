fb268e676ce4739759b0a09036b37a3c53c706a5
Martin K <mrtn.karlsson@gmail.com>
2020-01-13 23:19:22 +0100

    changed to sending objects instead

--- client.py
+++ client.py
@@ -7,17 +7,6 @@ height = 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
-clientNumber = 0
-
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
 
 def redraw_window(win, player, player2):
     win.fill((255, 255, 255))
@@ -29,19 +18,12 @@ def redraw_window(win, player, player2):
 def main():
     run = True
     n = Network()
-    start_pos = read_pos(n.get_pos())
-    p = Player(start_pos[0], start_pos[1], 100, 100, (0, 255, 0))
-    p2 = Player(0, 0, 100, 100, (255, 0, 0))
-
+    p = n.get_p()
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
-
-        p2pos = read_pos(n.send(make_pos((p.x, p.y))))
-        p2.x = p2pos[0]
-        p2.y = p2pos[1]
-        p2.update()
+        p2 = n.send(p)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -51,4 +33,5 @@ def main():
         redraw_window(win, p, p2)
 
 
-main()
+if __name__ == '__main__':
+    main()
